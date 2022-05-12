5a8b5daab9d49ef7c5d24d6c1c45f874cf2e057c
Jalal999 <jmammadli2021@ada.edu.az>
2022-05-12 16:24:48 +0300

    navbar-commit-1

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,9 +1,30 @@
+import Link from 'next/link'
+import Card from '@mui/material/Card';
+import { Nav, MenuLinks, Logo, MenuLink } from "./NavbarStyle"
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <Card>
+            <Nav>
+                <Logo>
+                    <Link href='/'>
+                        BookStore
+                    </Link>
+                </Logo>
+                <MenuLinks>
+                    <MenuLink>
+                        <Link href='/cart'>
+                            <ShoppingCartIcon color='primary' />
+                        </Link>
+                    </MenuLink>
+                    <MenuLink>
+                        <MenuIcon color='primary'/>
+                    </MenuLink>
+                </MenuLinks>
+            </Nav>
+        </Card>
     )
 }
 
