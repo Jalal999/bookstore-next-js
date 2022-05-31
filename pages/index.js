54ac42fee32a783f056e2f09ab9fab0fe66ec4a5
Jalal999 <jmammadli2021@ada.edu.az>
2022-05-31 17:29:22 +0300

    api-fix

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ products }) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("/api/products");
   return {
     props: {
       products: res.data,
