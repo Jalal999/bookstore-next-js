7546d70edeef2b5b687d16632af2c56d40ceb11e
Jalal999 <jmammadli2021@ada.edu.az>
2022-06-02 14:50:47 +0300

    last

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://demo-bookstore.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://bookstoredemo.vercel.app';
