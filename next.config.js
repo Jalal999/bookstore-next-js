ab4ebf9ba09f25767e1d0d3bbccca275cad5fc2e
Jalal999 <jmammadli2021@ada.edu.az>
2022-06-07 16:19:32 +0300

    make it better

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   env: {
-    "APP_ENV_BASE_URL": "https://my-demo-bookstore.herokuapp.com",
-    "APP_ENV_MONGO_URL": "mongodb+srv://Jalal999:Jalal999@cluster0.m9bd3.mongodb.net/bookstoree?retryWrites=true&w=majority"
+    "BASE_URL": "https://jalal-demo.herokuapp.com",
+    "MONGO_URL": "mongodb+srv://Jalal999:Jalal999@cluster0.m9bd3.mongodb.net/bookstoree?retryWrites=true&w=majority"
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import ProductList from '../components/ProductList'
 import axios from 'axios'
-import {server} from '../config';
-import { env } from process
 
 export default function Home({ products }) {
   return (
@@ -12,8 +10,8 @@ export default function Home({ products }) {
 }
 
 export const getServerSideProps = async () => {
-  console.log(process.env.APP_ENV_BASE_URL)
-  const res = await axios.get(`${env.APP_ENV_BASE_URL}/api/products`);
+  console.log(process.env.BASE_URL)
+  const res = await axios.get(`${process.env.BASE_URL}/api/products`);
   return {
     props: {
       products: res.data,
