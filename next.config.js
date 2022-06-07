870548aa5492fc8482899402c529c1785c9126b9
Jalal999 <jmammadli2021@ada.edu.az>
2022-06-07 13:41:31 +0300

    make it better

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   env: {
-    "BASE_URL": "https://next-react-bookstore.herokuapp.com",
+    "BASE_URL": "https://boookstore-demoo.herokuapp.com",
     "MONGO_URL": "mongodb+srv://Jalal999:Jalal999@cluster0.m9bd3.mongodb.net/bookstoree?retryWrites=true&w=majority"
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import ProductList from '../components/ProductList'
 import axios from 'axios'
 import {server} from '../config';
+import { env } from process
 
 export default function Home({ products }) {
   return (
@@ -12,7 +13,7 @@ export default function Home({ products }) {
 
 export const getServerSideProps = async () => {
   console.log(process.env.BASE_URL)
-  const res = await axios.get(`${process.env.BASE_URL}/api/products`);
+  const res = await axios.get(`${env.BASE_URL}/api/products`);
   return {
     props: {
       products: res.data,
